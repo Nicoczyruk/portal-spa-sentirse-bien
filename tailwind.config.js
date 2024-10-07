7e6ddc183a780ab74d03f24139c38bbf1ca39101
gonzahag <gonzahag@gmail.com>
2024-10-07 16:21:40 -0300

    Primeras interfaces de inicio y perfil

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // Especifica las rutas de tus archivos
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'spa-verde-claro': '#e9ffc7',
+        'spa-verde-oscuro': '#d0e1b5',
+      }
+    },
   },
   plugins: [],
 }
