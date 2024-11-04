2739ae8a79f234e8853a9120fa8789d26c1a13a9
Nicoczyruk <nicoczyruk2@gmail.com>
2024-11-03 22:12:43 -0300

    Fixes V3

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import { register } from './serviceWorkerRegistration';  // Registrar el SW
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';  // Registrar el SW
 
 // Import reportWebVitals (optional)
 import reportWebVitals from './reportWebVitals';
@@ -18,8 +18,18 @@ root.render(
 );
 
 // Register the service worker
-register();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (e.g., reportWebVitals(console.log))
 reportWebVitals();
+
+if ('Notification' in window && navigator.serviceWorker) {
+  Notification.requestPermission().then(function (permission) {
+    if (permission === 'granted') {
+      console.log('Permiso de notificaciones concedido.');
+    } else {
+      console.log('Permiso de notificaciones denegado.');
+    }
+  });
+}
